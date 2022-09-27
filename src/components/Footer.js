9fac3fbf854a20d893e5ae11c64d008ec8f51111
nikhil3113 <nikhilgovindchavan@ternaengg.ac.in>
2022-09-27 19:50:39 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Copytight &copy; 2022</p>
+        <a href="/about">About</a>
+    </footer>
+
+  )
+}
+
+export default Footer
---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+const Task = ({task, onDelete, onToggle}) => {
+  return (
+    <div className={`task ${task.reminder ? 
+    'reminder' : ''}`} onDoubleClick={()=>{
+      onToggle(task.id)
+    }}>
+        <h3>{task.text}{' '}
+        <FontAwesomeIcon icon={faTimes} style={{cursor: 'pointer'}}
+        onClick = {()=>onDelete(task.id)} 
+        /></h3>
+        <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
